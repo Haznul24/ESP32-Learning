65447195deb4aea683f7340caa66066e2841703c
unknown <abrarhaznul@gmail.com>
2026-05-17 11:44:02 +0700

    updated code

--- pushButtonHandling/src/main.cpp
+++ pushButtonHandling/src/main.cpp
@@ -1,28 +1,33 @@
 #include <Arduino.h>
 
-int pinButton1 = 18;    // Change Mode
-int pinButton2 = 19;    // Record Waypoint
+int pinButton1 = 18;
+int pinButton2 = 19;
 int pinButton3 = 5;
 
-bool rawB1 = false, lastRawB1 = false, stableB1 = false;
-bool rawB2 = false, lastRawB2 = false, stableB2 = false;
-bool rawB3 = false, lastRawB3 = false, stableB3 = false;
-unsigned long lastChangeB1 = 0, lastChangeB2 = 0, lastChangeB3 = 0;
-const unsigned long DEBOUNCE_MS = 10;
+unsigned long ignoreUntilB1 = 0;
+bool stateB1 = HIGH, prevStateB1 = HIGH;
+const unsigned long DEBOUNCE_MS = 100;
 
-void readButtons()  {
-  unsigned long currentTime = millis();
+bool readButtons()  {
+  unsigned long now = millis();
 
   // PB 1
-  rawB1 = (digitalRead(pinButton1)) == LOW;
-  if (rawB1 != lastRawB1) {
-    lastChangeB1 = currentTime;
-    lastRawB1 = rawB1;
+  if (now >= ignoreUntilB1 && digitalRead(pinButton1) != stateB1) {
+    ignoreUntilB1 = now + DEBOUNCE_MS;
+    stateB1 = !stateB1;
   }
-  else if (currentTime - lastChangeB1 > DEBOUNCE_MS) {
-    stableB1 = rawB1;
-  } 
 }
+
+bool handleButtons()  {
+  if (stateB1 != prevStateB1) {
+    prevStateB1 = stateB1;
+    if (stateB1 == LOW ){
+      Serial.println("Button 1 Pressed");
+    }
+  }
+}
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -31,8 +36,5 @@ void setup() {
 
 void loop() {
   readButtons();
-  if (stableB1) {
-    Serial.println("Button 1 Pressed");
-  }
-
+  handleButtons();
 }
